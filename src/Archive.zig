604f21550f9c04dafef1a7714d6a88070910955d
Jakub Konka <kubkon@jakubkonka.com>
2024-02-04 13:11:33 +0100

    adapt Archive parser to BSD/Darwin style

--- src/Archive.zig
+++ src/Archive.zig
@@ -42,31 +42,21 @@ pub fn parse(self: *Archive) !void {
 
         if (!mem.eql(u8, &hdr.ar_fmag, ARFMAG)) return error.InvalidHeaderDelimiter;
 
-        const size = try hdr.size();
+        var size = try hdr.size();
+        if (try hdr.nameLength()) |len| size -= len;
         defer {
             _ = stream.seekBy(size) catch {};
         }
 
-        if (hdr.isSymtab() or hdr.isSymtab64()) {
-            self.symtab.format = if (hdr.isSymtab64()) .p64 else .p32;
-            try self.symtab.parse(self.gpa, self.data[stream.pos..][0..size]);
-            continue;
-        }
-        if (hdr.isStrtab()) {
-            self.strtab = self.data[stream.pos..][0..size];
-            continue;
-        }
-        if (hdr.isSymdef() or hdr.isSymdefSorted()) {
+        const name = try hdr.getName(self.gpa, reader);
+        if (mem.eql(u8, name, SYMDEF) or
+            mem.eql(u8, name, SYMDEF64) or
+            mem.eql(u8, name, SYMDEF_SORTED) or
+            mem.eql(u8, name, SYMDEF64_SORTED))
+        {
             // TODO
             continue;
         }
-
-        const name = if (hdr.name()) |name|
-            try self.gpa.dupe(u8, name)
-        else if (try hdr.nameOffset()) |off|
-            try self.gpa.dupe(u8, self.getString(off))
-        else
-            unreachable;
 
         const gop = try self.objects.getOrPut(self.gpa, pos);
         assert(!gop.found_existing);
@@ -159,26 +149,6 @@ const ar_hdr = extern struct {
         return std.fmt.parseInt(u32, value, 10);
     }
 
-    fn isStrtab(self: ar_hdr) bool {
-        return mem.eql(u8, &self.ar_name, STRNAME);
-    }
-
-    fn isSymtab(self: ar_hdr) bool {
-        return mem.eql(u8, &self.ar_name, SYMNAME);
-    }
-
-    fn isSymtab64(self: ar_hdr) bool {
-        return mem.eql(u8, &self.ar_name, SYM64NAME);
-    }
-
-    fn isSymdef(self: ar_hdr) bool {
-        return mem.eql(u8, &self.ar_name, SYMDEFNAME);
-    }
-
-    fn isSymdefSorted(self: ar_hdr) bool {
-        return mem.eql(u8, &self.ar_name, SYMDEFSORTEDNAME);
-    }
-
     fn name(self: *const ar_hdr) ?[]const u8 {
         const value = &self.ar_name;
         if (mem.startsWith(u8, value, "#1/")) return null;
@@ -186,11 +156,24 @@ const ar_hdr = extern struct {
         return value[0..sentinel];
     }
 
-    fn nameOffset(self: ar_hdr) !?u32 {
+    fn nameLength(self: ar_hdr) !?u32 {
         const value = &self.ar_name;
         if (!mem.startsWith(u8, value, "#1/")) return null;
         const trimmed = mem.trimRight(u8, self.ar_name["#1/".len..], &[_]u8{0x20});
         return try std.fmt.parseInt(u32, trimmed, 10);
+    }
+
+    /// Caller owns the memory.
+    fn getName(self: *const ar_hdr, gpa: Allocator, reader: anytype) ![]const u8 {
+        if (self.name()) |n| return n;
+        if (try self.nameLength()) |len| {
+            const buf = try gpa.alloc(u8, len);
+            defer gpa.free(buf);
+            try reader.readNoEof(buf);
+            const actual_len = mem.indexOfScalar(u8, buf, @as(u8, 0)) orelse len;
+            return try gpa.dupe(u8, buf[0..actual_len]);
+        }
+        return error.MalformedArchive;
     }
 
     pub fn format(
@@ -290,19 +273,12 @@ const Symtab = struct {
     };
 };
 
-fn genSpecialMemberName(comptime name: []const u8) *const [16]u8 {
-    assert(name.len <= 16);
-    const padding = 16 - name.len;
-    return name ++ &[_]u8{0x20} ** padding;
-}
-
 const ARMAG = "!<arch>\n";
 const ARFMAG = "`\n";
-const SYMNAME = genSpecialMemberName("/");
-const STRNAME = genSpecialMemberName("//");
-const SYM64NAME = genSpecialMemberName("/SYM64/");
-const SYMDEFNAME = genSpecialMemberName("__.SYMDEF");
-const SYMDEFSORTEDNAME = genSpecialMemberName("__.SYMDEF SORTED");
+const SYMDEF = "__.SYMDEF";
+const SYMDEF64 = "__.SYMDEF_64";
+const SYMDEF_SORTED = "__.SYMDEF SORTED";
+const SYMDEF64_SORTED = "__.SYMDEF_64 SORTED";
 
 const assert = std.debug.assert;
 const fat = @import("fat.zig");
